5ed966bb0ef2fd5d1c8d016eba7bc37909a4fa21
Tukkin Moriarty <mrtukkin@gmail.com>
2016-10-24 00:39:02 +0200

    Added front page

---
+++ front-page.php
@@ -0,0 +1,44 @@
+<?php get_header(); ?>
+<section class="intro" role="main">
+	<h1>Willkommen!</h1>
+
+	<div class="dashaus row">
+		<div class="iconog small-12 medium-5 columns" ><img src="<?php echo get_bloginfo('template_url') ?>/assets/images/model_wb_ca.png"></div>
+		<div class="small-12 medium-7 columns" >
+			<h2>Das Haus</h2>
+			<p>Start by adding an element with a class of .row. This will create a horizontal block to contain vertical columns. Then add elements with a .column class within that row. You can use .column or .columns—the only difference is grammar. Specify the widths of each column with the .small-#, .medium-#, and .large-# classes.</p>
+		</div>
+	</div>
+
+	<div class="bildungskonzept row">
+		<div class="small-12 medium-7 columns" >
+			<h2>Das Bildungskonzept</h2>
+			<p>Start by adding an element with a class of .row. This will create a horizontal block to contain vertical columns. Then add elements with a .column class within that row. You can use .column or .columns—the only difference is grammar. Specify the widths of each column with the .small-#, .medium-#, and .large-# classes.</p>
+		</div>
+		<div class="iconog small-12 medium-5 columns" ></div>
+	</div>
+
+	<div class="nachhaltigkeit selbstverwaltung row">
+		<div class="iconog small-6 medium-5 large-3 columns" ></div>
+		<div class="small-6 medium-7 large-3 columns" >
+			<h2>Nachhaltigkeit</h2>
+			<p>Start by adding an element with a class of .row. This will create a horizontal block to contain vertical columns. Then add elements with a .column class within that row. You can use .column or .columns—the only difference is grammar. Specify the widths of each column with the .small-#, .medium-#, and .large-# classes.</p>
+		</div>
+		<div class="small-6 medium-7 large-3 columns" >
+			<h2>Selbstverwaltung</h2>
+			<p>Start by adding an element with a class of .row. This will create a horizontal block to contain vertical columns. Then add elements with a .column class within that row. You can use .column or .columns—the only difference is grammar. Specify the widths of each column with the .small-#, .medium-#, and .large-# classes.</p>
+		</div>
+		<div class="iconog small-6 medium-5 large-3 columns" ></div>
+	</div>
+
+	<div class="unterstützung row">
+		<div class="iconog small-12 columns" ></div>
+		<div class="small-12 columns" >
+			<h2>Uns unterstützen!</h2>
+			<p>Start by adding an element with a class of .row. This will create a horizontal block to contain vertical columns. Then add elements with a .column class within that row. You can use .column or .columns—the only difference is grammar. Specify the widths of each column with the .small-#, .medium-#, and .large-# classes.</p>
+		</div>
+	</div>
+
+</section>
+
+<?php get_footer(); ?>
